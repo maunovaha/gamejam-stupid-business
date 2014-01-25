0606683e42089315902832b25eb3d9b62012428f
poppanoxi <noxious_productions@hotmail.com>
2014-01-25 21:25:21 +0200

    fixeds!

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -16,29 +16,30 @@ define(['game'], function(Game) {
 	 */
 	App.prototype.init = function() {
 
-	var backgroundmusic = new Audio('audio/background.mp3'); 
-	if (typeof myAudio.loop == 'boolean')
-	{
-		backgroundmusic.loop = true;
-	}
-	else
-	{
-		backgroundmusic.addEventListener('ended', function() {
-			this.currentTime = 0;
-			this.play();
-		}, false);
-	}
-
-	backgroundmusic.play();
+			background = new Audio('audio/background.ogg'); 
+		if (typeof background.loop == 'boolean')
+		{
+			background.loop = true;
+		}
+		else
+		{
+			background.addEventListener('ended', function() {
+				this.currentTime = 0;
+				this.play();
+			}, false);
+		}
+		background.play();
 	
 		if(document.URL.indexOf("game") !== -1) {
-
+			
 			var game = new Game();
 			game.init();
 
 			return;
 
 		}
+		
+	
 
 		return console.log("App init index view..");
 
